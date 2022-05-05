c37373084bfa5477eca4ca353874912d4a8d3d47
govindgiri2021 <87941029+govindgiri2021@users.noreply.github.com>
2022-05-05 16:05:04 +0530

    Update namespace.sh

--- namespace.sh
+++ namespace.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-# sample value for your variables
 MYVARVALUE=$1
 
-# read the yml template from a file and substitute the string
-# {{MYVARNAME}} with the value of the MYVARVALUE variable
 template=`cat "namespace.yml" | sed "s/{{MYVARNAME}}/$MYVARVALUE/g"`
 echo "$template" | kubectl apply -f -
 
 template=`cat "deployment.yaml" | sed "s/{{MYVARNAME}}/$MYVARVALUE/g"`
-
-# apply the yml with the substituted value
 echo "$template" | kubectl apply -f -
