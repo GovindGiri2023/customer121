fe6aeb788230d48407bd81a4d3129f123342878b
govindgiri2021 <87941029+govindgiri2021@users.noreply.github.com>
2022-05-05 18:27:32 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash 
 git init .
-touch .gitignore
-git add --all
-git commit -m"initial commit"
 
 git remote add origin $1
 git branch $2
-//git push origin --all
-//git push origin --tags
 
 echo "Done."    
