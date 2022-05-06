8a3e15a116295da6bcc79f8b9d59f1b4cdc4f111
govindgiri2021 <87941029+govindgiri2021@users.noreply.github.com>
2022-05-06 19:46:38 +0530

    Update namespace.sh

--- namespace.sh
+++ namespace.sh
@@ -7,6 +7,6 @@ echo "$template" | kubectl apply -f -
 template=`cat "deployment.yaml" | sed "s/{{MYNAMESPACE}}/$MYNAMESPACEVALUE/g"`
 echo "$template" | kubectl apply -f -
 
-MYVARVALUE=$2 
-template=`cat "cafe-ingress.yaml" | sed "s/{{MYNAMESPACE}}/$MYNAMESPACEVALUE/g"`
+DOMAINVALUE=$2 
+template=`cat "cafe-ingress.yaml" | sed "s/{{MYNAMESPACE}}/$MYNAMESPACEVALUE/g;s/{{DOMAIN}}/$DOMAINVALUE/g"`
 echo "$template" | kubectl apply -f -
