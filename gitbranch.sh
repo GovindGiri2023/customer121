61a2d3687f5401e01102d921dcc89e954ca3248a
govindgiri2021 <87941029+govindgiri2021@users.noreply.github.com>
2022-05-09 13:30:48 +0530

    Update gitbranch.sh

--- gitbranch.sh
+++ gitbranch.sh
@@ -1,29 +1,6 @@
-username=`git config github.user`
-password=$1
-if [ "$username" = "" ]; then
-    echo "Could not find username, run 'git config --global github.user <username>'"
-    invalid_credentials=1
-fi
-
-# get repo name
-dir_name=`basename $(pwd)`
-read -p "Do you want to use '$dir_name' as a repo name?(y/n)" answer_dirname
-case $answer_dirname in
-  y)
-    # use currently dir name as a repo name
-    reponame=$dir_name
-    ;;
-  n)
-    read -p "Enter your new repository name: " reponame
-    if [ "$reponame" = "" ]; then
-        reponame=$dir_name
-    fi
-    ;;
-  *)
-    ;;
-esac
-
-
+username=$1
+password=$2
+reponame=$3
 # create repo
 echo "Creating Github repository '$reponame' ..."
 curl -u $username:$password https://api.github.com/user/repos -d '{"name":"'$reponame'"}'
