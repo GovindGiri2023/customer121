f4e648e075e57805eddab4b0faed5d089b267a2d
govindgiri2021 <87941029+govindgiri2021@users.noreply.github.com>
2022-05-09 12:03:33 +0530

    Update SG_Update.sh

--- SG_Update.sh
+++ SG_Update.sh
@@ -1,1 +1,1 @@
-aws ec2 authorize-security-group-ingress --group-id sg-0ff006212f3364d74 --protocol tcp --port 3306 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id sg-0ff006212f3364d74 --protocol tcp --port $1 --cidr 0.0.0.0/0
