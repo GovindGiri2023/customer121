78e882b2e06e2ba63107c766b8f3c72a6c9bbb45
govindgiri2021 <87941029+govindgiri2021@users.noreply.github.com>
2022-05-09 12:12:36 +0530

    Update namespace.sh

--- namespace.sh
+++ namespace.sh
@@ -4,7 +4,8 @@ MYNAMESPACEVALUE=$1
 template=`cat "namespace.yml" | sed "s/{{MYNAMESPACE}}/$MYNAMESPACEVALUE/g"`
 echo "$template" | kubectl apply -f -
 
-template=`cat "deployment.yaml" | sed "s/{{MYNAMESPACE}}/$MYNAMESPACEVALUE/g"`
+PORTVALUE=$3
+template=`cat "deployment.yaml" | sed "s/{{MYNAMESPACE}}/$MYNAMESPACEVALUE/g;s/{{PORT}}/$PORTVALUE/g"`
 echo "$template" | kubectl apply -f -
 
 DOMAINVALUE=$2 
